ea2fb2cd0dfab6cceacd4a9020caa48a88a38697
manojkumar <mano2991@users.noreply.github.com>
2025-02-22 11:16:24 +0530

    Add files via upload

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -31,3 +31,22 @@ for (let i = 0; i < totalBodySkin; i++) {
 document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
     document.querySelector('.style-switcher').classList.toggle('open');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.querySelector("#theme-toggle");
+    const body = document.body;
+
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark");
+    }
+
+    themeToggle.addEventListener("click", function() {
+        body.classList.toggle("dark");
+
+        if (body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
